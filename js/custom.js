23cb6701df520095e86ad76c2f54c8568d910df6
codingshreyash <shreyashran@gmail.com>
2022-04-02 22:44:47 -0400

    timer and photo changes

--- js/custom.js
+++ js/custom.js
@@ -158,3 +158,32 @@
     });
 
 })(jQuery);
+var end = new Date('04/12/2022 12:30 PM');
+
+    var _second = 1000;
+    var _minute = _second * 60;
+    var _hour = _minute * 60;
+    var _day = _hour * 24;
+    var timer;
+
+    function showRemaining() {
+        var now = new Date();
+        var distance = end - now;
+        if (distance < 0) {
+
+            clearInterval(timer);
+            document.getElementById('countdown').innerHTML = 'VOTING IS OPEN NOW! VOTE FOR SHREYASH!!';
+            return;
+        }
+        var days = Math.floor(distance / _day);
+        var hours = Math.floor((distance % _day) / _hour);
+        var minutes = Math.floor((distance % _hour) / _minute);
+        var seconds = Math.floor((distance % _minute) / _second);
+
+        document.getElementById('countdown').innerHTML = days + ' days ';
+        document.getElementById('countdown').innerHTML += hours + ' hrs ';
+        document.getElementById('countdown').innerHTML += minutes + ' mins ';
+        document.getElementById('countdown').innerHTML += seconds + ' secs left, till voting booths open!';
+    }
+
+    timer = setInterval(showRemaining, 1000);
